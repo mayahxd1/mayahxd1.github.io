9eba61bde8c1b5781cbe5e0850688900a3af652f
Maya Cizina <mcizina@mail.weber.edu>
2024-08-04 00:55:39 -0600

    change name

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a');
 
     for (const link of links) {
-        link.addEventListener('click', smoothScroll);
-    }
-
-    function smoothScroll(event) {
-        event.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: 'smooth'
+        link.addEventListener('click', function(event) {
+            links.forEach(link => link.classList.remove('active'));
+            this.classList.add('active');
         });
     }
 });
